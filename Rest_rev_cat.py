0eb70efad54b482ecd5cbe7ac926e88869e6d351
udit kumar <uditkumar489@gmail.com>
2017-11-24 07:48:55 +0530

    data cleaned successfully
    all the unneccesary words @ clutters removed

--- Rest_rev_cat.py
+++ Rest_rev_cat.py
@@ -8,3 +8,19 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
 
+# Cleaning the texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0, 1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
