f6e13b3208b47239df398aa8bf068a07af1e6523
udit kumar <uditkumar489@gmail.com>
2017-11-24 07:47:26 +0530

    initial commit
    libs @ ds imported

---
+++ Rest_rev_cat.py
@@ -0,0 +1,10 @@
+# Natural Language Processing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+
